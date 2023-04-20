06ac446cb936b42afdc8741bbf77920de9edb582
Tim Balvers <128842895+tbalvers@users.noreply.github.com>
2023-04-20 10:22:01 -0400

    Create dynamic grid Grid responds to user input to grow and shrink

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,71 @@
 let container = document.querySelector('.container');
-for(let i = 0; i < 16; i++)
+let input = document.querySelector('#input');
+let inputLog = [];
+input.addEventListener('input', e => {
+    console.log(e.data);
+    if (Number.isInteger(e.data / 1) && (Math.sign(e.data / 1) >= 0) && e.data)
+    {
+        inputLog.unshift(e.data);
+        console.log(inputLog);
+        let parsedNum = 0
+        counter = 1
+        inputLog.forEach(log => {
+            parsedNum += (log * counter);
+            counter *= 10;
+        })
+        if (parsedNum <=100)
+        {
+            createGrid(parsedNum);
+        }
+        else
+        {
+            input.value = '';
+            inputLog = [];
+            deleteGrid();
+            let p = document.querySelector('#warning');
+            p.textContent = 'Number must be less than 100';
+        }
+    }
+    else if (e.data === 'null')
+    {
+        input.value = '';
+        inputLog = [];
+        deleteGrid();
+    }
+    else
+    {
+        input.value = '';
+        inputLog = [];
+        deleteGrid();
+    }
+})
+
+function createGrid (parsedNum)
 {
-    div = document.createElement('div');
-    div.classList.add('grid');
-    div.textContent = 'hi';
-    container.appendChild(div);
+    deleteGrid()
+    let p = document.querySelector('#warning');
+    p.textContent = '';
+    let sqrt = Math.sqrt(parsedNum);
+    console.log(sqrt);
+    for (let i = 0; i < parsedNum; i++)
+    {
+        div = document.createElement('div');
+        div.classList.add('grid');
+        div.textContent = 'hi';
+        div.style.width = `${90 / sqrt}vw`
+        div.style.height = `${70 / sqrt}vh`
+        container.appendChild(div);
+    }
 }
+
+function deleteGrid ()
+{
+    while (container.firstChild)
+    {
+        container.removeChild(container.firstChild);
+    }
+}
+
 
 let grid = document.querySelectorAll('.grid');
 
